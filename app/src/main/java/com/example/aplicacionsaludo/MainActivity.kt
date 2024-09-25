fcad1b550d51cb84e6d27d917ec19f13b14fc31e
vmigdie12 <victordemiguel10@gmail.com>
2024-09-25 23:06:00 +0200

    subida

---
+++ app/src/main/java/com/example/aplicacionsaludo/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.aplicacionsaludo
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val boton = findViewById<Button>(R.id.buttonImprime)
+        boton.setOnClickListener{
+            val nombre = findViewById<EditText>(R.id.nameValue).text.toString()
+            Snackbar.make(boton,"Hola $nombre", Snackbar.LENGTH_LONG).show()
+        }
+    }
+}
